ec259bcceaa692e90fdbbc6fce14010d7851d7ec
thataxelguy <axel.woermann@gmail.com>
2026-08-28 10:44:41 +0200

    bring animal data into new animal.html file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,20 +7,37 @@ def load_data(filepath):
 
 animals_data = load_data('animals_data.json')
 
+# open animals_template html and store it in template
+with open("animals_template.html", 'r') as handle:
+    template = handle.read()
+
+# loop through json and store wanted data in output string
+output: str = ""
 for animal in animals_data:
     name = animal.get('name')
     characteristics = animal['characteristics']
     locations = animal['locations']
 
     if name:
-        print("Name: " + name)
+        output += f"Name:  {name}\n"
 
     if characteristics.get('diet') is not None:
-        print("Diet: " + characteristics.get('diet'))
+        output += f"Diet:  {characteristics['diet']}\n"
 
     if locations:
-        print("Location: " + locations[0])
+        output += f"Location:  {locations[0]}\n"
 
     if characteristics.get('type') is not None:
-        print("Type: " + characteristics.get('type'))
-    print("\n")
+        output += f"Type:  {characteristics['type']}\n"
+    output += "\n"
+
+# replace string in template with output and store in final_html
+final_html = template.replace(
+    "__REPLACE_ANIMALS_INFO__",
+    output
+)
+
+# write final_html to animals.html
+with open("animals.html", 'w') as handle:
+    handle.write(final_html)
+
